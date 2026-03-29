ad7870ca505d98e7b753c0674a48678a94edb728
unknown <ron.garcia.santos@gmail.com>
2026-03-29 17:38:40 +0800

    feat(ui): Set timer in 60 seconds and adjust the font colors

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,5 +1,6 @@
 import ToggleSwitch from '../atoms/ToggleSwitch'
 import { useTheme } from '../../contexts/ThemeContext'
+import { useState, useEffect } from 'react';
 
 /**
  * 032920260409 - Task
@@ -10,10 +11,20 @@ import { useTheme } from '../../contexts/ThemeContext'
 const Header = () => {
   const { theme } = useTheme()
   const isDark = theme === 'dark'
+  const [seconds, setSeconds ]  = useState(60)
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setSeconds((prev) => (prev > 0 ? prev - 1 : 60))
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <header
-      className={`px-4 py-1 h-16 grid grid-cols-[1fr_auto_1fr] items-center relative  ${isDark ? 'border-white/40' : 'border-black/20'} shadow-sm  ${isDark ? 'shadow-white/20': ''} `}
+      className={`px-4 py-1 h-16 grid grid-cols-[1fr_auto_1fr]  items-center relative  ${isDark ? 'border-white/40' : 'border-black/20'} shadow-md  ${isDark ? 'shadow-green-900/70': ''} `}
     >
         <div className="">
           <h1 className="col-span-3 text-4xl font-bold">
@@ -21,11 +32,10 @@ const Header = () => {
           </h1>
         </div>
         
-        
-        <div className="bg-green-400/40 px-10 h-20 self-start flex justify-center items-center shadow-lg shadow-green-900 [clip-path:polygon(0_0,100%_0,85%_100,15%_100%)]">
-          <div className="flex flex-col items-center leading-none ">
-              <span className="text-xl ">
-                Sunny
+        <div className="">
+          <div className="flex flex-col items-center leading-none">
+              <span className="text-4xl font-bold">
+                {seconds.toString().padStart(2, '0')}
               </span>
           </div>
         </div>
