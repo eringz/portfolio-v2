3ec6fc5535abc75e144044aadb7935f12325e194
unknown <ron.garcia.santos@gmail.com>
2026-03-30 21:25:53 +0800

    feat(components): abstract theme and toggle method for better code

--- src/components/atoms/ToggleSwitch.tsx
+++ src/components/atoms/ToggleSwitch.tsx
@@ -1,17 +1,17 @@
 import { motion } from 'framer-motion'
 import { useTheme } from '../../contexts/ThemeContext'
 
-export type ThemeMode = 'light' | 'dark'
+// export type ThemeMode = 'light' | 'dark'
 
 const ToggleSwitch = () => {
-  const { theme, setTheme } = useTheme()
+  const { theme, toggleTheme } = useTheme()
   const isDark = theme === 'dark'
 
   return (
     <motion.button
       whileHover={{ scale: 1.01 }}
       whileTap={{ scale: 0.99 }}
-      onClick={() => setTheme(isDark ? 'light' : 'dark')}
+      onClick={toggleTheme}
       className={`p-1 w-16 h-6 flex ${isDark ? 'bg-green-900' : 'bg-gray-300'} ${isDark ? 'justify-end' : 'justify-start'} items-center rounded-full cursor-pointer transition-colors duration-600`}
     >
       <motion.div
@@ -22,8 +22,11 @@ const ToggleSwitch = () => {
           damping: 20,
           mass: 1.2,
         }}
-        className={`flex m-1 h-4 w-8 ${isDark ? 'bg-gray-300' : 'bg-black/60'} rounded-full shadow-md`}
-      />
+        className={`flex h-4 w-8 text-center ${isDark ? 'bg-gray-300' : 'bg-black/60'} rounded-full shadow-md`}
+      >
+        {/* {theme === 'light' ? '☀️' : '🌙'} */}
+      </motion.div>
+
     </motion.button>
   )
 }
