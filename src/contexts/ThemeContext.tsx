23137e4a3ad1c33677e0286de963c6e3bcea2456
unknown <ron.garcia.santos@gmail.com>
2026-03-31 13:45:54 +0800

    feat(ui): debug and test the dynamic of theme

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -5,23 +5,41 @@ export type ThemeMode = 'light' | 'dark';
 type ThemeContextType = {
   theme: ThemeMode;
   setTheme: React.Dispatch<React.SetStateAction<ThemeMode>>;
+  toggleTheme: () => void;
 };
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<ThemeMode>('light');
+  const [theme, setTheme] = useState<ThemeMode>(() => {
+      const savedTheme = localStorage.getItem('theme') as ThemeMode;
+
+      if (savedTheme) return savedTheme;
+
+      const hour = new Date().getHours();
+
+      return (hour >= 19 || hour < 6) ? 'dark' : 'light';
+  });
+
+  // set toggle switch
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'))
+  }
 
   useEffect(() => {
+    
     if (theme === 'dark') {
       document.documentElement.classList.add('dark');
     } else {
       document.documentElement.classList.remove('dark');
     }
+
+
+    localStorage.setItem('theme', theme)
   }, [theme]);
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
