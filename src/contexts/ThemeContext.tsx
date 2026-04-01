03f3da11181b44296f920499e11ffcee9ede95ae
unknown <ron.garcia.santos@gmail.com>
2026-04-01 21:14:51 +0800

    feat(ui): refactor and add theme class to lay out effect

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useLayoutEffect } from 'react';
 
 export type ThemeMode = 'light' | 'dark';
 
@@ -12,31 +12,44 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = useState<ThemeMode>(() => {
+      // Checking the saved theme on your user's local storage
       const savedTheme = localStorage.getItem('theme') as ThemeMode;
-
       if (savedTheme) return savedTheme;
 
       const hour = new Date().getHours();
+      console.log(`Hour: ${hour}`)
 
       return (hour >= 19 || hour < 6) ? 'dark' : 'light';
+
   });
+
+  console.log(theme)
+
+
+  useLayoutEffect(() => {
+    const root = window.document.documentElement;
+
+    if (theme === 'dark') {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+
+    localStorage.setItem('theme', theme);
+
+    const timeout = setTimeout(() => {
+      root.classList.add('theme-ready')
+    }, 100)
+
+    return () => clearInterval(timeout)
+
+  }, [theme]);
+
 
   // set toggle switch
   const toggleTheme = () => {
     setTheme((prev) => (prev === 'light' ? 'dark' : 'light'))
   }
-
-  useEffect(() => {
-    
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-
-
-    localStorage.setItem('theme', theme)
-  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
