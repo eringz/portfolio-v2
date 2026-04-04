bbe4bddac9f7979496251ec2b9c773562c0f84c9
unknown <ron.garcia.santos@gmail.com>
2026-04-05 03:28:26 +0800

    feat(core): manage components and paths using react browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import './index.css'
 import App from './App.tsx'
+import Home from './components/sections/Home'
+import AboutBento from './components/sections/AboutMe.tsx' 
+import ProjectBento from './components/sections/Projects.tsx' 
+import Contact from './components/sections/Contact' 
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutBento />} />
+          <Route path="/projects" element={<ProjectBento />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
   </StrictMode>,
 )
