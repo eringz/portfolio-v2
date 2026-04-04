54fe582e29ffe6cbab412bbba898bc45ab84d27a
unknown <ron.garcia.santos@gmail.com>
2026-04-04 09:19:32 +0800

    feat(ui): making a proper alignment for better navigation ui

---
+++ src/components/shared/Nav.tsx
@@ -0,0 +1,11 @@
+import RadialNav from '../elements/RadialNav'
+
+const Nav = () => {
+    return (
+        <div className="flex justify-center p-2">
+            <RadialNav />
+        </div>
+    )
+}
+
+export default Nav
