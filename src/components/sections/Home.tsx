3bc3e9aacfd44da414c6902b25c37c50c7d49c76
unknown <ron.garcia.santos@gmail.com>
2026-04-06 06:59:04 +0800

    feat(ui section): updated the Home page blocks for alignment and content imporovement

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -1,8 +1,16 @@
 import AndroidSimulator from '../blocks/HomeBlocks/AndroidSimulator'
+import Terminal from '../blocks/HomeBlocks/Terminal';
 
 const Home = () => {
     return (
-        <AndroidSimulator />
+        <div className="flex justify-between px-24 h-full w-full">
+            <div className="flex justify-center items-center  ">
+                <Terminal />
+            </div>
+            <div className="m-8 h-full w-124 flex justify-center items-center ">
+                <AndroidSimulator />
+            </div>
+        </div>
     )
 }
 
