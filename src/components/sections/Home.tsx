1324cc085b6dca2a8e98e64268c15efea5d7e0fa
unknown <ron.garcia.santos@gmail.com>
2026-04-08 10:52:43 +0800

    feat(ui): minimal adjustment for ui enhancement and alignment

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -1,15 +1,23 @@
 import AndroidSimulator from '../blocks/HomeBlocks/AndroidSimulator'
 import Terminal from '../blocks/HomeBlocks/Terminal';
+import GithubActivity from '../blocks/HomeBlocks/GithubActivity';
 
 const Home = () => {
+
     return (
-        <div className="flex justify-between px-24 h-full w-full">
-            <div className="flex justify-center items-center  ">
+        <div className="flex h-full w-full">
+            <div className="flex flex-col gap-4 p-24 w-1/2 items-center">
                 <Terminal />
+                <div className="p-4 w-full">
+
+                    <GithubActivity />
+                </div>
             </div>
-            <div className="m-8 h-full w-124 flex justify-center items-center ">
+            <div className="w-1/2 h-full flex flex-col justify-center items-center">
                 <AndroidSimulator />
             </div>
+            
+            
         </div>
     )
 }
