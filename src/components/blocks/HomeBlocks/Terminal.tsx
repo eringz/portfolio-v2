2ca3157f4bce7ca912a072a1e971ee0a5c8a4b58
unknown <ron.garcia.santos@gmail.com>
2026-04-09 04:19:17 +0800

    feat(component blocks): ongoing adjustments for terminal as homepage hero section

--- src/components/blocks/HomeBlocks/Terminal.tsx
+++ src/components/blocks/HomeBlocks/Terminal.tsx
@@ -1,26 +1,32 @@
 import { FaRegWindowMinimize } from "react-icons/fa";
 import { FaRegSquare } from "react-icons/fa6";
 import { MdClose } from "react-icons/md";
+import ronPhoto from "../../../assets/ron.png";
 
 
 const Terminal = () => {
-    const gray = "#434444"
+    
+
     return (
         <div
-            className={`w-200 h-120 bg-black border-3 border-[#434444] rounded-lg text-white/70`}
+            className={`w-180 h-84 bg-black border-3 border-[#434444] rounded-lg text-white/70`}
         >
             {/** HEADER */}
             <div className={`w-full h-1 bg-[#434444]`}></div>
             <div className="flex">
                 <div className="w-8 h-8 bg-[#434444]"></div>
                 <div className="flex items-center gap-2 px-4 py-2 w-64 h-8 bg-black rounded-full">
-                    <div className="bg-white/40 h-full w-4 h-4 rounded-full"></div>
+                    <div className="flex justify-center items-center bg-white/40 w-4 h-4 rounded-full">
+                        <img 
+                            src={ronPhoto}
+                            alt="Ron picture"
+                        />
+                    </div>
                     <span className="text-white">Daily Routine</span>
                 </div>
                 <div className="flex justify-end px-4 py-1 gap-8 w-full h-8 bg-[#434444]">
                     <span className="">
                         <FaRegWindowMinimize />
-                        <span></span>
                     </span>
                     <span>
                         <FaRegSquare />
