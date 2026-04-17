77c521c02e1099a9be839794990a7264a4c4e4d2
unknown <ron.garcia.santos@gmail.com>
2026-04-17 23:35:08 +0800

    update

--- src/features/terminal/data/terminalConfig.ts
+++ src/features/terminal/data/terminalConfig.ts
@@ -1,9 +1,9 @@
 export const terminalLines = [
     {label: "whoami", value: "Ron Santos (Computer Engineer)", type: "command"},
-    {label: "status", value: '"App Development"', type: "info"},
-    {label: "active_project", value: '"Fixed Asset Inventory v1.0"', type: "info"},
-    {label: "location", value: '"Makati City, PH"', type: "info"},
-    {label: "availability", value: '"Open for Collaboration"', type: "info"}
+    {label: "status", value: "App Development", type: "info"},
+    {label: "active_project", value: "Fixed Asset Inventory v_01", type: "info"},
+    {label: "location", value: "Makati City", type: "info"},
+    {label: "availability", value: "Open for Collaboration", type: "info"}
 ];
 
 export const terminalSettings = {
