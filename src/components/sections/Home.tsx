ba49ea39286220bec30799c90ccd3658e9bbe0a3
unknown <ron.garcia.santos@gmail.com>
2026-04-18 09:36:58 +0800

    feat(shared): minor adjustment for home page

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -1,25 +1,23 @@
 import AndroidSimulator from '../blocks/HomeBlocks/AndroidSimulator'
-import Terminal from '../blocks/HomeBlocks/Terminal';
-import GithubActivity from '../blocks/HomeBlocks/GithubActivity';
+import Terminal from '@/features/terminal/Terminal'
+
+import GithubActivity from '@/features/github/components/GithubActivity';
+
+
 
 const Home = () => {
 
     return (
         <div className="flex h-full w-full">
-            <div className="flex flex-col gap-4 p-24 w-1/2 items-center">
+            <div className="flex flex-col gap-16 p-24 w-1/2 justify-between items-center">
                 <Terminal />
-                <div className="p-4 w-full">
-
-                    <GithubActivity />
-                </div>
+                {/* <GithubActivity /> */}
+                <GithubActivity />
             </div>
             <div className="w-1/2 h-full flex flex-col justify-center items-center">
                 <AndroidSimulator />
             </div>
-            
-            
         </div>
     )
 }
-
 export default Home
