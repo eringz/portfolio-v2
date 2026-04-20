b540d22eef35e47bb083795f419775ef342dad07
unknown <ron.garcia.santos@gmail.com>
2026-04-20 23:15:19 +0800

    features: additional of device data in terminal

---
+++ src/features/terminal/device/DeviceSpecs.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import type { DeviceSpecs } from '../types/index.ts';
+
+const getEasyOS = () => {
+  const platform = navigator.platform.toLowerCase();
+  if (platform.includes('win')) return 'Windows';
+  if (platform.includes('mac')) return 'macOS';
+  if (platform.includes('linux')) return 'Linux';
+  if (/android/.test(navigator.userAgent.toLowerCase())) return 'Android';
+  if (/iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase())) return 'iOS';
+  return 'Unknown OS';
+};
+
+const DeviceSpecs = () => {
+    const [specs, setSpecs] = useState<DeviceSpecs | null>(null);
+    const osName = getEasyOS();
+    
+    useEffect(() => {
+
+        const getSpecs = async () => {
+            const battery = await (navigator as any).getBattery?.();
+      
+            setSpecs({
+                os: navigator.platform,
+                cores: navigator.hardwareConcurrency || "Unknown",
+                ram: (navigator as any).deviceMemory ? `${(navigator as any).deviceMemory}GB` : "Unknown",
+                screen: `${window.screen.width}x${window.screen.height}`,
+                battery: battery ? `${Math.round(battery.level * 100)}%` : "N/A",
+            });
+        };
+
+        console.log(navigator);
+
+        getSpecs();
+
+    }, []);
+
+
+    return (
+        <div className="flex flex-col p-4 w-48 lg:w-128 font-mono rounded-lg">
+            <p> Your system Specs...</p>
+            {/* <p> OS: {specs?.os}</p> */}
+            <p> OS: {osName}</p>
+            <p> CPU CORES: {specs?.cores}</p>
+            <p> RAM: ~{specs?.ram}</p>
+            <p> RESOLUTION: {specs?.screen}</p>
+            <p> BATTERY: {specs?.battery}</p>
+        </div>
+    )
+}
+
+export default DeviceSpecs;
