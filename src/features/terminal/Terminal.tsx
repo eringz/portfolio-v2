7f153aa28783d3b2e7f2ac2efd88ac971558c2aa
unknown <ron.garcia.santos@gmail.com>
2026-04-20 23:10:49 +0800

    features: made an adjustment for terminal contents

--- src/features/terminal/Terminal.tsx
+++ src/features/terminal/Terminal.tsx
@@ -1,7 +1,7 @@
 import "./assets/terminal.css";
 import { terminalLines, terminalSettings } from './data/terminalConfig';
 import { TerminalRevealElement } from './components/TerminalReveal';
-import { motion } from 'framer-motion';
+import DeviceSpecs from './device/DeviceSpecs';
 import ronPhoto from "@/assets/ron.png";
 import { useTheme } from "@/contexts/ThemeContext";
 
@@ -12,7 +12,7 @@ const Terminal = () => {
     const isDark = theme === 'dark';
 
     return (
-        <div className={`p-6 w-full ${isDark ? 'dark-terminal-window' : 'terminal-window'}  rounded-lg font-mono text-sm shadow-lg transition duration-1000`}>
+        <div className={`p-6 mt-120 lg:mt-0 w-full text-xs lg:text-lg ${isDark ? 'dark-terminal-window' : 'terminal-window'}  rounded-lg font-mono text-sm shadow-lg transition duration-1000`}>
             {/** HEADER */}
             <div className="flex justify-center items-center gap-2 p-4 mb-2 border-b border-green-900/20">
                 <div className="flex gap-1.5">
@@ -22,32 +22,34 @@ const Terminal = () => {
                 </div>
             </div>
 
-            <div className="flex justify-between">
+            <div className="flex justify-between items-end">
                 <div className="space-y-3">
-                    {terminalLines.map((line, index) => (
+                    <DeviceSpecs />
+                    {/* {terminalLines.map((line, index) => (
                         <div
                             key={index}
                             className="flex flex-wrap items-center gap-x-2"
                         >
-                            {/* <span>
+                            
+                            <span>
                                 {line.type === "command" ? "λ" : ">"} {line.label}
-                            </span> */}
+                            </span>
                             <TerminalRevealElement 
                                 text={line.value}
                                 delay={terminalSettings.lineStagger * index}
                             />
-                            {/* <TerminalContent 
+                            <TerminalContent 
                                 text="Ronasfjsdjfs;kjfsdkfj"
                                 delay={1}
-                            /> */}
+                            />
                         </div>
-                    ))}
+                    ))} */}
                 </div>
-                <div className="flex flex-col items-center">
+                <div className="w-full">
                     <img 
                         src={ronPhoto}
                         alt="ron"
-                        className="w-40 h-40 rounded-full border"
+                        className="w-12 h-12 lg:w-40 lg:h-40  rounded-full border"
                     />
                 </div>
  
